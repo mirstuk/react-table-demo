6616c17bce1a7e5e3981cca4aadc90425f335ee8
Francesco Mirabella <mirstuk@users.noreply.github.com>
2020-10-15 09:01:25 +0200

    React Table Tutorial - 5 - Header Groups

--- src/components/columns.js
+++ src/components/columns.js
@@ -40,3 +40,58 @@ export const COLUMNS = [
     accessor: 'phone',
   },
 ];
+
+export const GROUPED_COLUMNS = [
+  {
+    Header: 'Id',
+    Footer: 'Id',
+    accessor: 'id',
+  },
+  {
+    Header: 'Name',
+    Footer: 'Name',
+    columns: [
+      {
+        Header: 'First Name',
+        Footer: 'First Name',
+        accessor: 'first_name',
+      },
+      {
+        Header: 'Last Name',
+        Footer: 'Last Name',
+        accessor: 'last_name',
+      },
+    ],
+  },
+  {
+    Header: 'Email',
+    Footer: 'Email',
+    accessor: 'email',
+  },
+  {
+    Header: 'Date of Birth',
+    Footer: 'Date of Birth',
+    accessor: 'date_of_birth',
+  },
+  {
+    Header: 'Age',
+    Footer: 'Age',
+    accessor: 'age',
+  },
+  {
+    Header: 'Info',
+    Footer: 'Info',
+    columns: [
+      {
+        Header: 'Country',
+        Footer: 'Country',
+        accessor: 'country',
+      },
+      {
+        Header: 'Phone',
+        Footer: 'Phone',
+        accessor: 'phone',
+      },
+    ],
+  },
+];
