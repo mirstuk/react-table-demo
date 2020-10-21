07521507bbb7680652bc5573b6e704bed0f6b0d2
Francesco Mirabella <mirstuk@users.noreply.github.com>
2020-10-21 16:43:51 +0200

    React Table Tutorial - 10 - More on Filtering

--- src/components/GlobalFilter.js
+++ src/components/GlobalFilter.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useAsyncDebounce } from 'react-table';
 
 const GlobalFilter = ({ filter, setFilter }) => {
+  const [value, setValue] = useState(filter);
+
+  const onChange = useAsyncDebounce(value => {
+    setFilter(value || undefined);
+  }, 400);
+
   return (
     <div className="search">
       Global Search:
-      <input value={filter || ''} onChange={e => setFilter(e.target.value)} />
+      <input
+        value={value || ''}
+        onChange={e => {
+          setValue(e.target.value);
+          onChange(e.target.value);
+        }}
+      />
     </div>
   );
 };
--- src/components/columns.js
+++ src/components/columns.js
@@ -6,25 +6,22 @@ export const COLUMNS = [
     Header: 'Id',
     Footer: 'Id',
     accessor: 'id',
-    Filter: ColumnFilter,
+    disableFilters: true,
   },
   {
     Header: 'First Name',
     Footer: 'First Name',
     accessor: 'first_name',
-    Filter: ColumnFilter,
   },
   {
     Header: 'Last Name',
     Footer: 'Last Name',
     accessor: 'last_name',
-    Filter: ColumnFilter,
   },
   {
     Header: 'Email',
     Footer: 'Email',
     accessor: 'email',
-    Filter: ColumnFilter,
   },
   {
     Header: 'Date of Birth',
@@ -33,25 +30,21 @@ export const COLUMNS = [
     Cell: ({ value }) => {
       return format(new Date(value), 'dd/MM/yyyy');
     },
-    Filter: ColumnFilter,
   },
   {
     Header: 'Age',
     Footer: 'Age',
     accessor: 'age',
-    Filter: ColumnFilter,
   },
   {
     Header: 'Country',
     Footer: 'Country',
     accessor: 'country',
-    Filter: ColumnFilter,
   },
   {
     Header: 'Phone',
     Footer: 'Phone',
     accessor: 'phone',
-    Filter: ColumnFilter,
   },
 ];
 
